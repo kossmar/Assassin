7909f1010dc5968f6d5d2be64f84fc84dd90da97
Mar Koss <mar.koss@icloud.com>
2021-01-15 14:06:55 -0800

    cleaned up and commented code sections in nav-bar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -37,35 +37,30 @@ export default function NavBar({ page }) {
             <nav className="bg-white">
                 <div className="mx-auto px-2">
                     <div className="relative flex items-center justify-between h-20">
+
+                        {/* <!-- Mobile menu button--> */}
                         <div onClick={handleMenuClick} className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            {/* <!-- Mobile menu button--> */}
                             <div className={(menuDropdownOpen ? "transition transform -rotate-180" : "transition transform rotate-0")}>
                                 <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
                                     <span className="sr-only">Open main menu</span>
-                                    {/* Icon when menu is closed. 
-          
-            Heroicon name: menu
 
-            Menu open: "hidden", Menu closed: "block"
-           */}
+                                    {/* Icon when menu is CLOSED. */}
                                     <svg className={"h-6 w-6 " + (menuDropdownOpen ? "hidden" : "block")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                                     </svg>
-                                    {/* <!-- Icon when menu is open. -->
-          <!--
-            Heroicon name: x
 
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
-
+                                    {/* Icon when menu is OPEN. */}
                                     <svg className={"h-6 w-6 " + (menuDropdownOpen ? "block" : "hidden")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
+
                                 </button>
                             </div>
                         </div>
 
                         <div className="flex-1 flex pl-5 items-center justify-center sm:items-stretch sm:justify-start">
+
+                            {/* LOGO */}
                             <Link href='/'>
                                 <a>
                                     <div className="cursor-pointer flex-shrink-0 flex items-center pl-16 sm:p-0">
@@ -73,8 +68,9 @@ export default function NavBar({ page }) {
                                         <img className="hidden lg:block h-10 w-auto ml-2" src="/images/assassin-logo-title.png" alt="Workflow" />
                                     </div>
                                 </a>
-
                             </Link>
+
+                            {/* NAV LINKS */}
                             <div className="hidden sm:block sm:m-auto pl-20 md:pl-44 lg:pl-14">
                                 <div className="flex space-x-4">
                                     <Link href="/rules">
@@ -94,33 +90,36 @@ export default function NavBar({ page }) {
                                     </Link>
                                 </div>
                             </div>
+
                         </div>
 
-                        {/* <!-- Profile dropdown --> */}
-                        <div>
-                            <div onClick={handleProfileClick} className={"p-1 rounded-full hover:bg-gray-400 hover:text-white " + (profileDropdownOpen ? "border-black outline-none" : "border-white hover:border-black")}>
-                                <div className="flex items-center justify-center">
-                                    <div className="px-2 hidden md:block">
-                                        <div className={"italic font-bold " + (profileDropdownOpen ? "transition transform rotate-3" : "transition transform rotate-0")}>MR. BIBBLZ</div>
-                                    </div>
-                                    <div className="px-2">
-                                        <div className="bg-gray-800 flex text-sm rounded-full ring-2 ring-black" id="user-menu" aria-haspopup="true">
-                                            <span className="sr-only">Open user menu</span>
-                                            <img className={"h-8 w-8 rounded-full " + (profileDropdownOpen ? "transition transform -rotate-180" : "transition transform rotate-0")} src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                        </div>
+                        {/* PROFILE BUTTON */}
+                        <div onClick={handleProfileClick} className={"p-1 rounded-full hover:bg-gray-400 hover:text-white " + (profileDropdownOpen ? "border-black outline-none" : "border-white hover:border-black")}>
+                            <div className="flex items-center justify-center">
+
+                                {/* Profile NAME */}
+                                <div className="px-2 hidden md:block">
+                                    <div className={"italic font-bold " + (profileDropdownOpen ? "transition transform rotate-3" : "transition transform rotate-0")}>MR. BIBBLZ</div>
+                                </div>
+
+                                {/* Profile IMAGE */}
+                                <div className="px-2">
+                                    <div className="bg-gray-800 flex text-sm rounded-full ring-2 ring-black" id="user-menu" aria-haspopup="true">
+                                        <span className="sr-only">Open user menu</span>
+                                        <img className={"h-8 w-8 rounded-full " + (profileDropdownOpen ? "transition transform -rotate-180" : "transition transform rotate-0")} src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                     </div>
                                 </div>
+
                             </div>
                         </div>
+
                     </div>
                 </div>
 
-                {/* <!--
-    Mobile menu, toggle classNamees based on menu state.
-
-    Menu open: "block", Menu closed: "hidden"
-  --> */}
+                {/* DROPDOWN MENUS */}
                 <div className="flex justify-between mx-auto">
+
+                    {/* PROFILE Menu */}
                     <div className={"sm:hidden " + (menuDropdownOpen ? "transition ease-in duration-100 transform opacity-100 scale-y-100 block" : "hidden transition ease-in duration-75 transform opacity-0 scale-y-0")}>
                         <div className="px-2 pt-2 pb-3 space-y-1">
                             <a href="/" className="bg-gray-900 text-white hover:bg-red-600 hover:shadow-xl-red block px-3 py-2 rounded-md text-base font-medium">New Campaign</a>
@@ -129,6 +128,7 @@ export default function NavBar({ page }) {
                         </div>
                     </div>
 
+                    {/* NAV Menu */}
                     <div className={"relative sm:absolute right-0 mr-0 ml-auto text-right " + (profileDropdownOpen ? "transition ease-in duration-100 transform opacity-100 scale-y-100 block" : "hidden transition ease-in duration-75 transform opacity-0 scale-y-0")}>
                         <div className="px-2 pt-2 pb-3 space-y-1">
                             <a href="/" className="text-gray-400 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Profile</a>
@@ -136,7 +136,9 @@ export default function NavBar({ page }) {
                             <a href="/about" className="text-gray-400 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign Out</a>
                         </div>
                     </div>
+
                 </div>
+
             </nav>
         </div>
     )
