780d025581abe43717440403bce36dadd1e148f7
Mar Koss <mar.koss@icloud.com>
2021-01-20 17:37:47 -0800

    added isForAssassins and inviteLinks props to inviteComponent

--- components/Invite.jsx
+++ components/Invite.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Invite({ props }) {
+export default function Invite({ isForAssassins=true, inviteLink="https://assassingame.com/game/invite/aSdfkj" }) {
     return (
         <div>
             <div>
                 <div className="flex flex-col justify-center w-96 mx-auto py-16">
                     <div className='flex flex-col'>
-                        <label className='text-left'>Invite Assassins</label>
+                        {(isForAssassins ? <label className='text-left'>Invite Assassins</label> : <label className='text-left'>Invite Moderator</label>)}
                         <input onChange name='campaignName' className='border my-2 pl-2' type="text" placeholder='e-mail' />
                     </div>
                     <div className='flex flex-col'>
                         <label className='text-left'>Invite Link</label>
                         <div onChange name='weapons' className='border rounded my-2 pl-2 py-2'>
-                            https://assassingame.com/game/invite/aSdfkj
+                            {inviteLink}
                         </div>
                     </div>
                 </div>
