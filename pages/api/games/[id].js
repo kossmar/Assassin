25bae70fb3872217d901f46ce13ea5bb532e302f
Mar Koss <mar.koss@icloud.com>
2021-01-26 17:06:31 -0800

    .

---
+++ pages/api/games/[id].js
@@ -0,0 +1,56 @@
+import dbConnect from '../../../utils/dbConnect'
+import Game from '../../../models/Game'
+
+export default async function handler(req, res) {
+    const {
+      query: { id },
+      method,
+    } = req
+  
+    await dbConnect()
+  
+    switch (method) {
+    //   case 'GET' /* Get a model by its ID */:
+    //     try {
+    //       const game = await Game.findById(id)
+    //       if (!game) {
+    //         return res.status(400).json({ success: false })
+    //       }
+    //       res.status(200).json({ success: true, data: game })
+    //     } catch (error) {
+    //       res.status(400).json({ success: false })
+    //     }
+    //     break
+  
+      case 'PUT' /* Edit a model by its ID */:
+        try {
+          const game = await Game.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+          })
+          if (!game) {
+            return res.status(400).json({ success: false })
+          }
+          res.status(200).json({ success: true, data: game })
+        } catch (error) {
+          res.status(400).json({ success: false })
+        }
+        break
+  
+    //   case 'DELETE' /* Delete a model by its ID */:
+    //     try {
+    //       const deletedPet = await Pet.deleteOne({ _id: id })
+    //       if (!deletedPet) {
+    //         return res.status(400).json({ success: false })
+    //       }
+    //       res.status(200).json({ success: true, data: {} })
+    //     } catch (error) {
+    //       res.status(400).json({ success: false })
+    //     }
+    //     break
+  
+      default:
+        res.status(400).json({ success: false })
+        break
+    }
+  }
