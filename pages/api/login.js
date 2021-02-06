2c05000e82f25816879cdf787111aa4e119381c6
Mar Koss <mar.koss@icloud.com>
2021-02-05 16:46:49 -0800

    was passing the whole user object into the Iron token sealer which included the image data, making the header way to fucking big. So parsed the username from the user returned from authentication and all seems to work decently now...

--- pages/api/login.js
+++ pages/api/login.js
@@ -22,9 +22,16 @@ export default nextConnect()
     try {
       const user = await authenticate('local', req, res)
       // session is the payload to save in the token, it may contain basic info about the user
-      const session = { ...user }
+      // const session = { ...user } THIS WAS THE ORIGINAL working code but it stopped working when I began adding photos
+      // From my understanding, I only need to pass username for token authentication. Keeping this for ref until I understand better
+      
+      // console.log("\n\n\nSESSION FROM USER: \n" + JSON.stringify(session._doc.username) + "\n\n\n")
+      const session = {username: user._doc.username}
 
       await setLoginSession(res, session)
+      // console.log("REZZZZ: " + res)
+
+      // console.log("SESSION - api/login: " + session)
 
       res.status(200).send({ done: true })
     } catch (error) {
