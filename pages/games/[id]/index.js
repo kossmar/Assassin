4c71c1fb140f06ecc4b132a2b7d199146ac8863f
Mar Koss <mar.koss@icloud.com>
2021-02-17 14:53:41 -0800

    forgot to save final change

--- pages/games/[id]/index.js
+++ pages/games/[id]/index.js
@@ -162,7 +162,7 @@ const GameComponent = ({ gameResult }) => {
             <Head>
                 <title>Assassin/Game/[id]</title>
             </Head>
-            
+
             <ConfirmationPopup
                 message={"Are you sure you want to delete this game?"}
                 isOpen={isConfirmDeleteOpen}
@@ -173,39 +173,7 @@ const GameComponent = ({ gameResult }) => {
                     deleteGame(game._id)
                 })}
             />
-
-            {/* <div className={(confirmationPopup.isOpen ? "fixed" : "hidden") + " bg-gray-200 bg-opacity-70 w-full h-full"}>
-                <div className="flex h-full w-full">
-                    <div className="flex p-2 mx-auto place-self-center bg-white border-2 border-gray-400 rounded-lg w-96 h-60">
-                        <div className=" mx-auto place-self-center bg-gray-200 space-y-4">
-                            <div className="text-center">
-                                Are you sure you want to delete this game?
-                            </div>
-                            <div className="grid grid-cols-2">
-                                <div onClick={(() => {
-                                    deleteGame(game._id)
-                                })} className={"my-2 cursor-pointer flex place-content-center w-36 h-10 rounded-md mx-auto border-red-400 bg-red-400 hover:bg-red-300 hover:border-2 text-white"}>
-                                    <div className="place-self-center">
-                                        YES
-                                    </div>
-                                </div>
-                                <div onClick={(() => {
-                                    setConfirmationPopup(prevValue => {
-                                        return {
-                                            ...prevValue,
-                                            isOpen: false
-                                        }
-                                    })
-                                })} className={"my-2 cursor-pointer flex place-content-center w-36 h-10 rounded-md mx-auto border-blue-400 bg-blue-400 hover:bg-blue-300 hover:border-2 text-white"}>
-                                    <div className="place-self-center">
-                                        CANCEL
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            
             <Layout page={page.rules}>
                 <section id="top">
 
